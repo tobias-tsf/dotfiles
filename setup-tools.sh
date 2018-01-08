a701f3b15edaa143f2d109e83312636654ca6800
Tobias Laber <tobias.laber.tsf@gmail.com>
2018-01-08 16:28:19 +0100

    Updated brewfile

--- setup-tools.sh
+++ setup-tools.sh
@@ -2,3 +2,15 @@
 
 # Install pip for python
 sudo easy_install pip
+
+# Install composer dependencies
+composer global require \
+  phpunit/phpunit \
+  squizlabs/php_codesniffer
+
+# Install vagrant plugins
+vagrant plugin install \
+  vagrant-vbguest \
+  vagrant-triggers \
+  vagrant-hostsupdater \
+  vagrant-gatling-rsync
